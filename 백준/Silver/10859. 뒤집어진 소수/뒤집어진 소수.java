45546c5a51e4403bc27d10f506dd980a5736548c
gyuoo <145361340+gyuoo@users.noreply.github.com>
2023-10-11 00:51:42 +0900

    [Silver II] Title: 뒤집어진 소수, Time: 1628 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.java
+++ 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.java
@@ -3,36 +3,29 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		char[] charN = br.readLine().toCharArray();
-		long longN = Long.parseLong(new String(charN));
-		if (longN == 1) {
-			System.out.println("no");
-			System.exit(0);
-		}
-		int len = charN.length;
-		char[] charN2 = new char[len];
-		for (int i = 0; i < len; i++) {
-			if (charN[i] == '3' || charN[i] == '4' || charN[i] == '7') {
-				System.out.println("no");
-				System.exit(0);
+		String str = br.readLine();
+		long N = Long.parseLong(str);
+		boolean flag = true;
+		if (!isPrime(N)) flag = false;
+		else {
+			char[] revN = new StringBuilder(str).reverse().toString().toCharArray();
+			for (int i = 0; i < revN.length; i++) {
+				if (revN[i] == '3' || revN[i] == '4' || revN[i] == '7') {
+					flag = false;
+					break;
+				}
+				else if (revN[i] == '6') revN[i] = '9';
+				else if (revN[i] == '9') revN[i] = '6';
 			}
-			else if (charN[len-i-1] == '6') charN2[i] = '9';
-			else if (charN[len-i-1] == '9')	charN2[i] = '6';
-			else charN2[i] = charN[len-i-1];
+			if (!isPrime(Long.parseLong(new String(revN)))) flag = false;
 		}
-		long longN2 = Long.parseLong(new String(charN2));
-		for (long i = 2; i*i <= longN; i++) {
-			if (longN % i == 0) {
-				System.out.println("no");
-				System.exit(0);
-			}
+		System.out.println(flag ? "yes" : "no");
+	}
+	static boolean isPrime(long n) {
+		if (n == 1) return false;
+		for (long i = 2; i*i <= n; i++) {
+			if (n % i == 0) return false;
 		}
-		for (long i = 2; i*i <= longN2; i++) {
-			if (longN2 % i == 0) {
-				System.out.println("no");
-				System.exit(0);
-			}
-		}
-		System.out.println("yes");
+		return true;
 	}
 }
