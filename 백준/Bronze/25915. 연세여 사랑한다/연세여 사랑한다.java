600cca6a593661a4c452170c6c1b279091658b1c
gyuoo <145361340+gyuoo@users.noreply.github.com>
2023-11-01 17:16:03 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] c = br.readLine().toCharArray();
+        System.out.println(Math.abs('I'-c[0])+84);
+    }
+}
