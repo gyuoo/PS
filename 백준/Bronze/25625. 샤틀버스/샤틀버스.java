c74b0f7a061654aae2592925e32f004ca95b6e75
gyuoo <145361340+gyuoo@users.noreply.github.com>
2023-11-18 23:47:12 +0900

    [Bronze IV] Title: 샤틀버스, Time: 76 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+        System.out.println(x > y ? x+y : y-x);
+    }
+}
