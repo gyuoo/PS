96ef43e86bd908835f675551dd7efe570282a429
gyuoo <145361340+gyuoo@users.noreply.github.com>
2023-11-23 19:02:15 +0900

    [Bronze II] Title: 점수 집계, Time: 76 ms, Memory: 11344 KB -BaekjoonHub

--- 백준/Bronze/9076. 점수 집계/점수 집계.java
+++ 백준/Bronze/9076. 점수 집계/점수 집계.java
@@ -12,10 +12,7 @@ public class Main {
             int[] arr = new int[5];
             for (int i = 0; i < 5; i++) arr[i] = Integer.parseInt(st.nextToken());
             Arrays.sort(arr);
-            arr[0] = arr[4] = 0;
-            if (arr[3]-arr[1] >= 4) sb.append("KIN");
-            else sb.append(arr[1]+arr[2]+arr[3]);
-            sb.append('\n');
+            sb.append(arr[3]-arr[1] < 4 ? arr[1]+arr[2]+arr[3] : "KIN").append('\n');
         }
         System.out.println(sb);
     }
