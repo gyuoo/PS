6bf0e000e1d868709dc6d11ad614b8525952f1e2
gyuoo <145361340+gyuoo@users.noreply.github.com>
2023-12-04 20:34:35 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+        System.out.println(M <= 2 ? "NEWBIE!" : N >= M ? "OLDBIE!" : "TLE!");
+    }
+}
