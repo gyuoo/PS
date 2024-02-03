46042341b0f8b70a493d2a81244096c72f4ac1f2
gyuoo <145361340+gyuoo@users.noreply.github.com>
2024-02-03 22:08:27 +0900

    [Gold V] Title: 0 만들기, Time: 84 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    static int N;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-->0) {
+            N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = i+1;
+            choice(1, 1, 0, true, "1");
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+    static void choice (int idx, int num, int sum, boolean flag, String str) {
+        if (idx == N) {
+            if (flag) sum += num;
+            else sum -= num;
+            if (sum == 0) sb.append(str).append('\n');
+            return;
+        }
+        int res = flag ? sum+num : sum-num;
+        choice(idx+1, num*10+idx+1, sum, flag, str+" "+(idx+1));
+        choice(idx+1, idx+1, res, true, str+"+"+(idx+1));
+        choice(idx+1, idx+1, res, false, str+"-"+(idx+1));
+    }
+}
