cbf9435e20dab8e634e1b28d3873cb62ef7e8c1f
gyuoo <145361340+gyuoo@users.noreply.github.com>
2024-02-17 23:14:15 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 80 ms, Memory: 11384 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append(":fan::fan::fan:\n:fan::").append(br.readLine()).append("::fan:\n:fan::fan::fan:");
+        System.out.print(sb);
+    }
+}
