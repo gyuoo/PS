113659c2b4ec2665ffc0678d1317d4e28a4db16b
gyuoo <145361340+gyuoo@users.noreply.github.com>
2024-04-18 23:00:37 +0900

    [Silver II] Title: N과 M (9), Time: 232 ms, Memory: 34268 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr, arr2;
+    static boolean[] used;
+    static StringBuilder sb;
+    static HashSet<String> hs;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        arr2 = new int[M];
+        used = new boolean[N];
+        hs = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        choice(0);
+        System.out.print(sb);
+    }
+
+    static void choice(int idx) {
+        if (idx == M) {
+            StringBuilder sb2 = new StringBuilder();
+            for (int i : arr2) sb2.append(i).append(' ');
+            if (!hs.contains(sb2.toString())) sb.append(sb2).append('\n');
+            hs.add(sb2.toString());
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            arr2[idx] = arr[i];
+            choice(idx+1);
+            used[i] = false;
+        }
+    }
+}
