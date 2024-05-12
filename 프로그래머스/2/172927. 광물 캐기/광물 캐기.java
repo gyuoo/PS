4a0990380d43a212bd2e4c375fefe06e69e6dab2
gyuoo <145361340+gyuoo@users.noreply.github.com>
2024-05-12 19:42:17 +0900

    [level 2] Title: 광물 캐기, Time: 0.47 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,60 +1,78 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
-    static class Mineral implements Comparable<Mineral>{
-        int diamond, iron, stone, sum;
-        Mineral (int diamond, int iron, int stone, int sum) {
-            this.diamond = diamond;
-            this.iron = iron;
-            this.stone = stone;
-            this.sum = sum;
+
+    /*
+     *  - 5개단위로 끊어서 Greedy하게 풀이
+     */
+
+    static class Info implements Comparable<Info>{
+        int[] minerals;
+        int cost;
+
+        Info(){
+            this.minerals = new int[5];
+            cost = 0;
         }
-        @Override
-        public int compareTo(Mineral o) {
-            if (this.sum == o.sum) {
-                if (this.diamond == o.diamond) {
-                    return Integer.compare(o.iron, this.iron);
-                }
-                return Integer.compare(o.diamond, this.diamond);
+
+        public int compareTo(Info i){
+            return i.cost-this.cost;
+        }
+
+        public int getCost(int div){
+            int ret = 0;
+            for(int i=0; i<5; i++){
+                if(minerals[i]==0) continue;
+
+                ret += Math.max(minerals[i]/div,1);
             }
-            return Integer.compare(o.sum, this.sum);
+
+            return ret;
         }
     }
+
+    static int getCost(String str){
+        switch(str){
+            case "diamond": return 25;
+            case "iron": return 5;
+            case "stone": return 1;
+        }
+        return 0;
+    }
+
     public int solution(int[] picks, String[] minerals) {
-        int ans = 0, len = minerals.length, canDig = 0;
-        for (int i : picks) canDig += i*5;
-        Mineral[] arr = new Mineral[len%5 == 0 ? len/5 : len/5+1];
-        if (canDig < len) arr = new Mineral[canDig/5];
-        Mineral m = new Mineral(0, 0, 0, 0);
-        for (int i = 0; i < Math.min(canDig, len); i++) {
-            if (minerals[i].equals("diamond")) {
-                m.diamond++;
-                m.sum += 25;
-            } else if (minerals[i].equals("iron")) {
-                m.iron++;
-                m.sum += 5;
-            } else {
-                m.stone++;
-                m.sum++;
-            }
-            if ((i+1)%5 == 0 || i == len-1) {
-                arr[i/5] = m;
-                m = new Mineral(0, 0, 0, 0);
-            }
-        }        
-        Arrays.sort(arr);
-        for (Mineral mineral : arr) {
-            if (0 < picks[0]) {
+        int answer = 0;
+
+        int sumPicks = picks[0] + picks[1] + picks[2];
+
+        Info[] info = new Info[sumPicks];
+        for(int i=0; i<info.length; i++){
+            info[i] = new Info();
+        }
+
+        for(int i=0; i<minerals.length && i<sumPicks*5; i++){
+            int cost = getCost(minerals[i]);
+            info[i/5].minerals[i%5] = cost;
+            info[i/5].cost += cost;
+        }
+
+        Arrays.sort(info);
+
+        for(int i=0; i<info.length; i++){
+            Info cur = info[i];
+
+            if(picks[0]>0){
+                answer += cur.getCost(25);
                 picks[0]--;
-                ans += mineral.diamond+mineral.iron+mineral.stone;
-            } else if (0 < picks[1]) {
+            } else if(picks[1]>0){
+                answer += cur.getCost(5);
                 picks[1]--;
-                ans += mineral.diamond*5+mineral.iron+mineral.stone;
-            } else if (0 < picks[2]) {
+            } else if(picks[2]>0){
+                answer += cur.getCost(1);
                 picks[2]--;
-                ans += mineral.sum;
             }
         }
-        return ans;
+
+        return answer;
     }
 }
