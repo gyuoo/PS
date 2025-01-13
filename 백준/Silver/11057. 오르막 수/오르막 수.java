a37ceb3e701937ea73b8da8a57ed908daf1b5467
Gyuyoung <145361340+gyuoo@users.noreply.github.com>
2025-01-13 20:03:37 +0900

    [Silver I] Title: 오르막 수, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N+1][10];
+        Arrays.fill(dp[0], 1);
+        for (int i = 1; i <= N; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j < 10; j++) {
+                dp[i][j] = (dp[i-1][j]%10007+dp[i][j-1]%10007)%10007;
+            }
+        }
+        System.out.print(dp[N][9]);
+    }
+}
