dd8103990c4960b4466ee2b8f6ad3d94e05d6422
Gyuyoung <145361340+gyuoo@users.noreply.github.com>
2026-02-25 15:14:16 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,31 +1,23 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int n, cnt;
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= 10; i++) {
+            dp[i] = dp[i-3]+dp[i-2]+dp[i-1];
+        }
         while (T-->0) {
-            n = Integer.parseInt(br.readLine());
-            arr = new int[]{1, 2, 3};
-            cnt = 0;
-            choice(0, 0);
-            sb.append(cnt).append('\n');
+            sb.append(dp[Integer.parseInt(br.readLine())]).append('\n');
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
-    static void choice(int num, int sum) {
-        if (sum == n) {
-            cnt++;
-            return;
-        }
-        if (num == n) return;
-        for (int i = 0; i < 3; i++) {
-            choice(num+1, sum+arr[i]);
-        }
-    }
+
 }
