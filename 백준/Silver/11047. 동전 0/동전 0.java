a9397c36e94971adfe914fd52233d364ef057994
Gyuyoung <145361340+gyuoo@users.noreply.github.com>
2026-03-12 13:20:06 +0900

    [Silver IV] Title: 동전 0, Time: 88 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int cnt = 0;
+        for (int i = N-1; 0 <= i; i--) {
+            cnt += K/arr[i];
+            K %= arr[i];
+        }
+        System.out.print(cnt);
+    }
+}
